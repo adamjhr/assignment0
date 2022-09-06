dfb6399960eba1c44c48d45ebde7156432828e1d
adamjhr <adam.jh.rose6@gmail.com>
2022-09-06 15:27:35 +0200

    Satisfies 400-year rule

--- ZerothAssignment/GregorianCalender.cs
+++ ZerothAssignment/GregorianCalender.cs
@@ -3,7 +3,7 @@ namespace ZerothAssignment;
 public class GregorianCalender {
 
     public bool IsLeapYear(int year) {
-        if (year == 0) return true;
+        if (year % 400 == 0) return true;
         if (year % 100 == 0) return false;
         return (year % 4) == 0;
     }
