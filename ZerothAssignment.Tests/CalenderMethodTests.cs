b1ed07f31d547aff112f408ca847c9be7582cc61
adamjhr <68515776+adamjhr@users.noreply.github.com>
2022-09-06 15:30:15 +0200

    Implemented is-leap-year method (#3)
    * Add 1 test
    
    * Add new tests to CalenderMethodTests
    
    * Change so 0 is considered a leap year
    
    * leap year function implemented to satisfy tests
    
    * Added tests for the centennial rule
    
    * Centennial rule satisfied
    
    * Added 400-year rule to tests
    
    * Fixed an incorrect test
    
    * Satisfies 400-year rule

--- ZerothAssignment.Tests/CalenderMethodTests.cs
+++ ZerothAssignment.Tests/CalenderMethodTests.cs
@@ -1,8 +1,115 @@
 namespace ZerothAssignment.Tests;
 
-public class UnitTest1
+public class CalenderMethodTests
 {
 
+    [Fact]
+    public void Returns_4_is_leap_year()
+    {
+        var calender = new GregorianCalender();
 
+        var result = calender.IsLeapYear(4);
+
+        result.Should().Be(true);
+    }
+
+    [Fact]
+    public void Returns_32_is_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(32);
+
+        result.Should().Be(true);
+    }
+
+    [Fact]
+    public void Returns_52_is_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(52);
+
+        result.Should().Be(true);
+    }
+
+    [Fact]
+    public void Returns_3_isnt_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(3);
+
+        result.Should().Be(false);
+    }
     
+    [Fact]
+    public void Returns_42_isnt_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(42);
+
+        result.Should().Be(false);
+    }
+
+    [Fact]
+    public void Returns_0_is_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(0);
+
+        result.Should().Be(true);
+    }
+
+    [Fact]
+    public void Returns_100_isnt_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(100);
+
+        result.Should().Be(false);
+    }
+
+    [Fact]
+    public void Returns_200_isnt_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(200);
+
+        result.Should().Be(false);
+    }
+
+    [Fact]
+    public void Returns_1700_isnt_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(1700);
+
+        result.Should().Be(false);
+    }
+
+    [Fact]
+    public void Returns_1600_is_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(1600);
+
+        result.Should().Be(true);
+    }
+
+    [Fact]
+    public void Returns_2400_is_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(2400);
+
+        result.Should().Be(true);
+    }
 }
