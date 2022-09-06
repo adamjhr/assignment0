9f86440aedbc23ce7649a0d938eacc923ef094dd
adamjhr <adam.jh.rose6@gmail.com>
2022-09-06 15:23:08 +0200

    Centennial rule satisfied

--- ZerothAssignment/GregorianCalender.cs
+++ ZerothAssignment/GregorianCalender.cs
@@ -3,6 +3,8 @@ namespace ZerothAssignment;
 public class GregorianCalender {
 
     public bool IsLeapYear(int year) {
+        if (year == 0) return true;
+        if (year % 100 == 0) return false;
         return (year % 4) == 0;
     }
 
