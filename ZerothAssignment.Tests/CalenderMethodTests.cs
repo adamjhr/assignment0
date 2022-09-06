5f5f07be5a74fd4bfc94ea45fda92bc5b6369878
adamjhr <adam.jh.rose6@gmail.com>
2022-09-06 15:11:05 +0200

    Add new tests to CalenderMethodTests

--- ZerothAssignment.Tests/CalenderMethodTests.cs
+++ ZerothAssignment.Tests/CalenderMethodTests.cs
@@ -12,5 +12,54 @@ public class CalenderMethodTests
 
         result.Should().Be(true);
     }
+
+    [Fact]
+    public void Returns_32_is_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(32);
+
+        result.Should().Be(true);
+    }
+
+    [Fact]
+    public void Returns_52_is_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(52);
+
+        result.Should().Be(true);
+    }
+
+    [Fact]
+    public void Returns_3_isnt_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(3);
+
+        result.Should().Be(false);
+    }
     
+    [Fact]
+    public void Returns_42_isnt_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(42);
+
+        result.Should().Be(false);
+    }
+
+    [Fact]
+    public void Returns_0_isnt_leap_year()
+    {
+        var calender = new GregorianCalender();
+
+        var result = calender.IsLeapYear(0);
+
+        result.Should().Be(false);
+    }
 }
