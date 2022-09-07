25df59308f6a4695f1d09e07e561a9000e1cf1e4
adamjhr <adam.jh.rose6@gmail.com>
2022-09-07 16:25:19 +0200

    Implemented working interface

--- ZerothAssignment/Program.cs
+++ ZerothAssignment/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace ZerothAssignment;
+
+internal class Program {
+    static void Main(string[] args) {
+        var calender = new GregorianCalender();
+
+        var input = Console.ReadLine();
+        if (Int32.TryParse(input, out int number)) {
+            if (calender.IsLeapYear(number)) {
+                Console.WriteLine("yay");
+            } else {
+                Console.WriteLine("nay");
+            }
+        } else {
+            Console.WriteLine("nay");
+        }
+    }
+}
