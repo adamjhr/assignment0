3247a5e7e9c26bf5aab63413b1da88773a138176
adamjhr <adam.jh.rose6@gmail.com>
2022-09-08 14:30:32 +0200

    Amended IsLeapYear to satisfy 1582 condition

--- ZerothAssignment/GregorianCalender.cs
+++ ZerothAssignment/GregorianCalender.cs
@@ -3,6 +3,7 @@ namespace ZerothAssignment;
 public class GregorianCalender {
 
     public bool IsLeapYear(int year) {
+        if (year < 1582) return false;
         if (year % 400 == 0) return true;
         if (year % 100 == 0) return false;
         return (year % 4) == 0;
